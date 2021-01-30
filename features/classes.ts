6e5acc823eba3a3b21efcb910d88995391d6b8ca
bryan kretz <kretzbryan@gmail.com>
2021-01-30 13:36:14 -0800

    commit of unsaved Class practice

--- features/classes.ts
+++ features/classes.ts
@@ -19,3 +19,14 @@ const car1 = new Car();
 
 car1.drive();
 car1.honk();
+
+// Instance Method Modifiers
+
+// Public
+// THis method can be called anywhere, any time.
+
+// Private
+// This method can only be called by other methods in this class.
+
+// Protected
+// This method can be called by other methods in this class, or by other methods in child classes.
